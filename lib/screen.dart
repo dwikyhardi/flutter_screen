fa12f3efdd0c814c987ce6805f5592149176ab44
Tomas Espeleta <tomas.espeleta@gmail.com>
2021-03-13 11:40:12 +0100

    migrated to null safety

--- lib/screen.dart
+++ lib/screen.dart
@@ -7,11 +7,11 @@ class Screen {
 
   static const MethodChannel _channel = MethodChannel('github.com/clovisnicolas/flutter_screen');
 
-  static Future<double> get brightness => _channel.invokeMethod<double>('brightness');
+  static Future<double?> get brightness => _channel.invokeMethod<double>('brightness');
 
   static Future<void> setBrightness(double brightness) => _channel.invokeMethod('setBrightness', <String, dynamic>{'brightness': brightness});
 
-  static Future<bool> get isKeptOn => _channel.invokeMethod<bool>('isKeptOn');
+  static Future<bool?> get isKeptOn => _channel.invokeMethod<bool>('isKeptOn');
 
   static Future<void> keepOn(bool on) => _channel.invokeMethod('keepOn', <String, dynamic>{'on': on});
 }
